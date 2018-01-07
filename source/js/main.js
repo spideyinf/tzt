f26c8c62ebb159834beef7fd0b5431519fc04856
spideyinf <spidey.inf@gmail.com>
2018-01-07 14:44:33 +0700

    p01-80

--- source/js/main.js
+++ source/js/main.js
@@ -24,4 +24,22 @@ $(function () {
       }
     }
   })
+
+  $('.p01-home__banner-slider').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  })
 })
