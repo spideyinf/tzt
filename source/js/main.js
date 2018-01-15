24200495da30cdcc8a80d3264365a1e448189773
spideyinf <spidey.inf@gmail.com>
2018-01-16 01:07:42 +0700

    p06-90

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,9 @@
 //=require bootstrap/dist/js/bootstrap.min.js
 //=require owl.carousel/dist/owl.carousel.min.js
 //=require nanoscroller/bin/javascripts/jquery.nanoscroller.js
+//=require jquery.rateit/scripts/jquery.rateit.min.js
+//=require slick-carousel/slick/slick.min.js
+//=require @zeitiger/elevatezoom/jquery.elevatezoom.js
 
 $(function() {
   'use strict';
@@ -99,4 +102,84 @@ $(function() {
       return !1;
     });
   })();
+
+  //See more - See less
+  $('.see-more').click(function() {
+    var $this = $(this);
+    $this.toggleClass('see-more');
+    $this.prev().collapse();
+    if ($this.hasClass('see-more')) {
+      $this.text('Xem thêm sản phẩm cùng nơi bán >');
+    } else {
+      $this.text('Thu gọn <');
+    }
+  });
+
+  /*!
+   * Bootstrap 4 multi dropdown navbar ( https://bootstrapthemes.co/demo/resource/bootstrap-4-multi-dropdown-navbar/ )
+   * Copyright 2017.
+   * Licensed under the GPL license
+   */
+
+  $(document).ready(function() {
+    $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+      var $el = $(this);
+      var $parent = $(this).offsetParent('.dropdown-menu');
+      if (
+        !$(this)
+          .next()
+          .hasClass('show')
+      ) {
+        $(this)
+          .parents('.dropdown-menu')
+          .first()
+          .find('.show')
+          .removeClass('show');
+      }
+      var $subMenu = $(this).next('.dropdown-menu');
+      $subMenu.toggleClass('show');
+
+      $(this)
+        .parent('li')
+        .toggleClass('show');
+
+      $(this)
+        .parents('li.nav-item.dropdown.show')
+        .on('hidden.bs.dropdown', function(e) {
+          $('.dropdown-menu .show').removeClass('show');
+        });
+
+      if (!$parent.parent().hasClass('navbar-nav')) {
+        $el
+          .next()
+          .css({ top: $el[0].offsetTop, left: $parent.outerWidth() - 4 });
+      }
+
+      return false;
+    });
+  });
+
+  // ElevateZoom Slider
+  $('#zoom_01').elevateZoom({
+    gallery: 'gallery',
+    cursor: 'pointer',
+    galleryActiveClass: 'active',
+    imageCrossfade: true
+  });
+
+  //pass the images to Fancybox
+  $('#zoom_01').bind('click', function(e) {
+    var ez = $('#zoom_01').data('elevateZoom');
+    $.fancybox(ez.getGalleryList());
+    return false;
+  });
+
+  $('.slider').slick({
+    dots: false,
+    vertical: true,
+    slidesToShow: 3,
+    slidesToScroll: 5,
+    verticalSwiping: true,
+    arrow: true
+  });
 });
